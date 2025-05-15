d1daf4860bf21be960a34edebbc375214bd3480e
hung148 <113944324+hung148@users.noreply.github.com>
2025-05-14 18:46:44 -0700

    Delete library-management-system/src/main/java/com/main/model directory

--- library-management-system/src/main/java/com/main/model/Member.java
+++
@@ -1,8 +0,0 @@
-package com.main.model;
-
-public class Member extends User {
-    // member is supended if balance is negative 
-    public Member(String email, String password, String username, String name, int id, String status, double balance) {
-        super(email, password, username, name, "member", id, status, balance);
-    }
-}
