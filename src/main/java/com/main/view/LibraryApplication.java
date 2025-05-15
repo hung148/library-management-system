b9da4ba25629d538ee84b7ca62a89379ba048fcc
coolFrogsss <149332728+nhat-uyen@users.noreply.github.com>
2025-05-15 00:18:40 -0700

    disable sizable login page - change style on both login and register to make it universal

--- src/main/java/com/main/view/LibraryApplication.java
+++ src/main/java/com/main/view/LibraryApplication.java
@@ -4,7 +4,6 @@ package com.main.view;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -16,11 +15,12 @@ public class LibraryApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException  {
         FXMLLoader login_loader =  new FXMLLoader(LibraryApplication.class.getResource("login-page.fxml"));
-        Scene scene = new Scene(login_loader.load());
+        Scene scene = new Scene(login_loader.load(),390, 562);
         URL cssURL = getClass().getResource("pageStyle.css");
         assert cssURL != null;
         scene.getStylesheets().add(cssURL.toExternalForm());
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
