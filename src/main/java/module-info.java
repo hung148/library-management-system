281c443448d16bc3d5d3ee69545bff0ad5fe11d8
Tom <trinhhungqt2004@gmail.com>
2025-05-21 19:01:10 -0700

    fix module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,4 +15,6 @@ module com.main {
     exports com.main.view;
     exports com.main.controller.admin;
     opens com.main.controller.admin to javafx.fxml;
+    opens com.main.controller.user to javafx.fxml;
+    exports com.main.controller.user;
 }
